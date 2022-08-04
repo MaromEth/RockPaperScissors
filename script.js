5edf28c48ead164fa5ea41eda363225a8880eba3
Ethan Marom <ethanmarom5@gmail.com>
2022-08-04 18:11:31 -0400

    formatted code and removed some testing lines

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let getComputerChoice = function(){
 }
 
 let playRound = function(playerSelection, computerSelection){
-    if (playerSelection === 'rock'){
+    if(playerSelection === 'rock'){
         if (computerSelection === 'rock'){
             return "You both chose rock, you tied!"
         } else if(computerSelection === 'paper'){
@@ -13,7 +13,7 @@ let playRound = function(playerSelection, computerSelection){
         } else {
             return "You won! Rock Beats Scissors"
         }
-    } else if(playerSelection === 'paper'){
+    }else if(playerSelection === 'paper'){
         if(computerSelection === 'rock'){
             return "You won! Paper beats Rock!"
         } else if(computerSelection === 'paper'){
@@ -24,7 +24,7 @@ let playRound = function(playerSelection, computerSelection){
     } else if(playerSelection === 'scissors'){
         if(computerSelection === 'rock'){
             return "You lose. Rock beats Scissors"
-      } else if (computerSelection === "paper"){
+      }else if(computerSelection === "paper"){
         return "You won! Scissors beats Paper!"
       }else{
         return "You tied! You both picked Scissors!"
@@ -45,25 +45,23 @@ let game = function(){
         let tie = result.includes('tied')
         console.log(win)
         console.log(tie)
-        if (win===true){
+        if(win===true){
             humanScore++
-        } else if(win===false && tie != true){
+        }else if(win===false && tie != true){
             computerScore++
-        } else {
+        }else{
             continue
         }
     
     }
-    if (humanScore > computerScore){
+    if(humanScore > computerScore){
         console.log(`The score was ${humanScore} you, ${computerScore} computer. You win!`)
-    } else if(humanScore < computerScore){
+    }else if(humanScore < computerScore){
         console.log(`The score was ${humanScore} you, ${computerScore} computer. You lose.`)
     }else{
         console.log(`The score was ${humanScore} you, ${computerScore} computer. You tied.`)
     }
     
 }
-string = 'abcdefg'
-upperCase = string.toUpperCase()
-let compchoice = getComputerChoice()
+
 game()
